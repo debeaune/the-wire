680f8f550950d08f586b76d4d899aa94e44cbdf0
Marie Laure Debeaune <ml.debeaune@gmail.com>
2026-09-06 17:46:23 +0200

    Add security headers

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
+header('X-Content-Type-Options: nosniff');
+header('X-Frame-Options: DENY');
+header('X-XSS-Protection: 1; mode=block');
+
 require_once __DIR__ . '/src/classes/Router.php';
 require_once __DIR__ . '/src/controllers/ArticleController.php';
 require_once __DIR__ . '/src/controllers/CommentController.php';
